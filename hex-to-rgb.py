ab88ebe0ef910c2b0e1f8411581125692fc04e1a
saladwithgrass <egod090809089@yandex.ru>
2024-12-21 18:34:38 +0300

    added percent option to hex-to-rgb

--- hex-to-rgb.py
+++ hex-to-rgb.py
@@ -6,9 +6,12 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('color', help='Hex color code. May be with or without the # in the beginning.')
     parser.add_argument('-s', '--separator', help='Separator between components', default=', ')
+    parser.add_argument('-p', '--percent', help='Will print precentages instead of 8-bit values', action='store_true')
     args = parser.parse_args()
     sep = args.separator
+    use_percent = args.percent
     color:str = args.color
+    max_value = 2**8
     color = color.removeprefix('#')
     match len(color):
         case 6:
@@ -21,7 +24,10 @@ def main():
     for pos in range(0, len(color), 2):
         if pos == len(color) - 2:
             sep = '\n'
-        print(int(color[pos:pos+2], 16), end=f'{sep}')
+        color_value = int(color[pos:pos+2], 16)
+        if use_percent:
+            color_value = int(color_value / max_value * 100)
+        print(color_value, end=f'{sep}')
 
 if __name__ == '__main__':
     main()
